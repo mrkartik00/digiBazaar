e8739a75a8cc9fb8fda26925ef9693aae5da0c0c
kartik <kartikgoutam7@gmail.com>
2024-08-27 15:19:36 +0530

    next config update

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		domains:[
+			'localhost',
+			'https://digibazaar-production.up.railway.app'
+		],
+	},
+};
+
+module.exports = nextConfig;
